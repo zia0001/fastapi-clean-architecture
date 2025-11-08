88ca2df6d9801750589dca256d93aae2f8747fb3
zia ud din <ziawc0001@gmail.com>
2025-11-08 23:36:33 +0500

    createpost through post method

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 
 app = FastAPI()
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "your posts"}
+    return {"data": "your post"}
+
+@app.post("/createpost")
+def create_post(payload: dict = Body()):
+    print(payload)
+    return {"post_data": f"title {payload['title']} content {payload['content']}"}
