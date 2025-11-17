4ed37330e72e3e80386c1ca93a868bc2658aac32
zia ud din <ziawc0001@gmail.com>
2025-11-18 00:28:51 +0500

    Implement update and delete post API endpoints

--- main.py
+++ main.py
@@ -59,8 +59,22 @@ def get_post (id: int, response: Response):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id {id} was not found")
         
             
-@app.delete("/posts/{id}")
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
    index = post_index(id)
+   if index == None:
+     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
    my_posts.pop(index)
-   return {"message": "post deletd successfully deleted"}
+   return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post:Post):
+    index = post_index(id)
+    if index == None:
+     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+    
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {'data': post_dict}
